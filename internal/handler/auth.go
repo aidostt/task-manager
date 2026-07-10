6ef70f2074be1364bac4df2253b2435b7b1933cd
aidostt <leket1999@gmail.com>
2026-07-10 18:57:55 +0500

    feat: create endpoint logic for user auth

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,6 +1,52 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"net/http"
 
-func (h *Handler) register(c *gin.Context) {}
-func (h *Handler) login(c *gin.Context)    {}
+	"github.com/aidostt/task-manager/internal/model"
+	"github.com/aidostt/task-manager/internal/service"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) register(c *gin.Context) {
+	var input model.RegisterInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input body"})
+		return
+	}
+	accessToken, refreshToken, err := h.services.User.RegisterUser(c, input.Email, input.Password)
+	if err != nil {
+		switch {
+		case errors.Is(err, service.ErrUserAlreadyExists):
+			c.JSON(http.StatusBadRequest, gin.H{"error": "user already exists"})
+			return
+		case errors.Is(err, service.ErrInvalidCredentials):
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input body"})
+			return
+		default:
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+			return
+		}
+	}
+	c.JSON(http.StatusCreated, gin.H{"token": accessToken, "refresh": refreshToken})
+}
+func (h *Handler) login(c *gin.Context) {
+	var input model.RegisterInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input body"})
+		return
+	}
+	accessToken, refreshToken, err := h.services.User.LoginUser(c, input.Email, input.Password)
+	if err != nil {
+		switch {
+		case errors.Is(err, service.ErrInvalidCredentials):
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input body"})
+			return
+		default:
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "internal server error"})
+			return
+		}
+	}
+	c.JSON(http.StatusCreated, gin.H{"token": accessToken, "refresh": refreshToken})
+}
